ec8f39be898ce5955360d95edaaf99d7de02aadf
Marigameo <mariappangameo@gmail.com>
2020-12-08 17:25:25 +0530

    slider UI complete w/o library

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,48 +1,246 @@
-import React, { Component } from "react";
-import Slider from "react-slick";
+import React, { Component } from 'react'
+import './Slider.css'
+import shirtImg from '../../assets/images/shirt.png'
+import pantsImg from '../../assets/images/pants.png'
 
-export default class MultipleItems extends Component {
+export default class Slider extends Component {
+    componentDidMount () {
+        this.slide();
+    }
+
+    slide () {
+        let wrapper = document.getElementById('slider'),
+            items = document.getElementById('slides');
+        let posX1 = 0,
+            posX2 = 0,
+            posInitial,
+            posFinal,
+            threshold = 100,
+            slides = items.getElementsByClassName('slide'),
+            slidesLength = slides.length,
+            slideSize = items.getElementsByClassName('slide')[0].offsetWidth,
+            firstSlide = slides[0],
+            lastSlide = slides[slidesLength - 1],
+            cloneFirst = firstSlide.cloneNode(true),
+            cloneLast = lastSlide.cloneNode(true),
+            index = 0,
+            allowShift = true;
+
+        // Clone first and last slide
+        items.appendChild(cloneFirst);
+        items.insertBefore(cloneLast, firstSlide);
+        wrapper.classList.add('loaded');
+
+        // Mouse events
+        items.onmousedown = dragStart;
+
+        // Touch events
+        items.addEventListener('touchstart', dragStart);
+        items.addEventListener('touchend', dragEnd);
+        items.addEventListener('touchmove', dragAction);
+
+        // Transition events
+        items.addEventListener('transitionend', checkIndex);
+
+        function dragStart (e) {
+            e = e || window.event;
+            e.preventDefault();
+            posInitial = items.offsetLeft;
+
+            if (e.type === 'touchstart') {
+                posX1 = e.touches[0].clientX;
+            } else {
+                posX1 = e.clientX;
+                document.onmouseup = dragEnd;
+                document.onmousemove = dragAction;
+            }
+        }
+
+        function dragAction (e) {
+            e = e || window.event;
+
+            if (e.type === 'touchmove') {
+                posX2 = posX1 - e.touches[0].clientX;
+                posX1 = e.touches[0].clientX;
+            } else {
+                posX2 = posX1 - e.clientX;
+                posX1 = e.clientX;
+            }
+            items.style.left = (items.offsetLeft - posX2) + "px";
+        }
+
+        function dragEnd (e) {
+            posFinal = items.offsetLeft;
+            if (posFinal - posInitial < -threshold) {
+                shiftSlide(1, 'drag');
+            } else if (posFinal - posInitial > threshold) {
+                shiftSlide(-1, 'drag');
+            } else {
+                items.style.left = (posInitial) + "px";
+            }
+
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+
+        function shiftSlide (dir, action) {
+            items.classList.add('shifting');
+
+            if (allowShift) {
+                if (!action) { posInitial = items.offsetLeft; }
+
+                if (dir === 1) {
+                    items.style.left = (posInitial - slideSize) + "px";
+                    index++;
+                } else if (dir === -1) {
+                    items.style.left = (posInitial + slideSize) + "px";
+                    index--;
+                }
+            };
+
+            allowShift = false;
+        }
+
+        function checkIndex () {
+            items.classList.remove('shifting');
+
+            if (index === -1) {
+                items.style.left = -(slidesLength * slideSize) + "px";
+                index = slidesLength - 1;
+            }
+
+            if (index === slidesLength) {
+                items.style.left = -(1 * slideSize) + "px";
+                index = 0;
+            }
+
+            allowShift = true;
+        }
+    }
+
     render () {
-        const settings = {
-            dots: false,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 3,
-            slidesToScroll: 3
-        };
         return (
-            <div>
-                <h2> Multiple items </h2>
-                <Slider {...settings}>
-                    <div>
-                        <h3>1</h3>
+            <div className="slider-container">
+                <p className="slider-heading"><span>LATEST COLLECTIONS</span></p>
+                <div id="slider" className="slider">
+                    <div className="wrapper">
+                        <div id="slides" className="slides">
+                            <div style={{ display: 'flex', flexDirection: "row" }} className="slide">
+                                <div className="slide-image">
+                                    <img src={shirtImg} alt="shirt image1"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={pantsImg} alt="pants image1"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={shirtImg} alt="shirt image2"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={pantsImg} alt="pants image2"></img>
+                                </div>
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: "row" }} className="slide">
+                                <div className="slide-image">
+                                    <img src={shirtImg} alt="shirt image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={pantsImg} alt="pants image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={shirtImg} alt="shirt image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={pantsImg} alt="pants image3"></img>
+                                </div>
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: "row" }} className="slide">
+                                <div className="slide-image">
+                                    <img src={shirtImg} alt="shirt image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={pantsImg} alt="pants image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={shirtImg} alt="shirt image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={pantsImg} alt="pants image3"></img>
+                                </div>
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: "row" }} className="slide">
+                                <div className="slide-image">
+                                    <img src={shirtImg} alt="shirt image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={pantsImg} alt="pants image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={shirtImg} alt="shirt image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={pantsImg} alt="pants image3"></img>
+                                </div>
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: "row" }} className="slide">
+                                <div className="slide-image">
+                                    <img src={shirtImg} alt="shirt image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={pantsImg} alt="pants image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={shirtImg} alt="shirt image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={pantsImg} alt="pants image3"></img>
+                                </div>
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: "row" }} className="slide">
+                                <div className="slide-image">
+                                    <img src={shirtImg} alt="shirt image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={pantsImg} alt="pants image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={shirtImg} alt="shirt image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={pantsImg} alt="pants image3"></img>
+                                </div>
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: "row" }} className="slide">
+                                <div className="slide-image">
+                                    <img src={shirtImg} alt="shirt image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={pantsImg} alt="pants image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={shirtImg} alt="shirt image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={pantsImg} alt="pants image3"></img>
+                                </div>
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: "row" }} className="slide">
+                                <div className="slide-image">
+                                    <img src={shirtImg} alt="shirt image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={pantsImg} alt="pants image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={shirtImg} alt="shirt image3"></img>
+                                </div>
+                                <div className="slide-image">
+                                    <img src={pantsImg} alt="pants image3"></img>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <h3>2</h3>
-                    </div>
-                    <div>
-                        <h3>3</h3>
-                    </div>
-                    <div>
-                        <h3>4</h3>
-                    </div>
-                    <div>
-                        <h3>5</h3>
-                    </div>
-                    <div>
-                        <h3>6</h3>
-                    </div>
-                    <div>
-                        <h3>7</h3>
-                    </div>
-                    <div>
-                        <h3>8</h3>
-                    </div>
-                    <div>
-                        <h3>9</h3>
-                    </div>
-                </Slider>
+                </div>
             </div>
-        );
+        )
     }
 }
