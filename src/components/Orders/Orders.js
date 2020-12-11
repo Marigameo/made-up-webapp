5f0a867d35f28188e51816167f7116b2872edf28
Marigameo <mariappangameo@gmail.com>
2020-12-11 17:23:49 +0530

    making orders page dynamic w/o hardcoding

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,6 +1,6 @@
 import React from 'react'
+import OrderItem from './OrderItem'
 import styles from './Orders.module.css'
-import denimModal from '../../assets/images/denim.png'
 
 export default function Orders () {
     return (
@@ -8,96 +8,7 @@ export default function Orders () {
             <div className={styles.heading}>Pending Orders</div>
             <div className={styles.ordersListWrapper}>
                 <ul className={styles.ordersList}>
-                    <li className={styles.listItem}>
-                        <div className={styles.image}>
-                            <img className={styles.image} src={denimModal} alt="denim-shirt-model"></img>
-                        </div>
-                        <div className={styles.details}>
-                            <div className={`${styles.orderId} + " " + ${styles.largeScreensOnly}`}>Order #12345678</div>
-                            <span className={styles.smallScreensOnly}>
-                                <div className={styles.listItem}>
-                                    <div className={styles.orderId}>Order #12345678</div>
-                                    <div className={styles.paymentType}>Cahs on delivery Rs.1899</div>
-                                </div>
-                            </span>
-                            <div className={styles.name}>Madeup White Cotton Blend Checkered Slim Fit Shirt</div>
-                            <div className={styles.deliveryDate}>Express Delivery by Sat, Aug 30</div>
-                        </div>
-                        <div className={styles.largeScreensOnly}>
-                            <div className={styles.paymentType}>Credit Card Payment</div>
-                            <div className={styles.amount}>Rs 1,899</div>
-                            <div className={styles.control}></div>
-                        </div>
-                    </li>
-                    <div className={styles.break}>
-                        <hr className={styles.linebreak} />
-                    </div>
-                    <li className={styles.listItem}>
-                        <div className={styles.image}>
-                            <img className={styles.image} src={denimModal} alt="denim-shirt-model"></img>
-                        </div>
-                        <div className={styles.details}>
-                            <div className={`${styles.orderId} + " " + ${styles.largeScreensOnly}`}>Order #12345678</div>
-                            <span className={styles.smallScreensOnly}>
-                                <div className={styles.listItem}>
-                                    <div className={styles.orderId}>Order #12345678</div>
-                                    <div className={styles.paymentType}>Cahs on delivery Rs.1899</div>
-                                </div>
-                            </span>
-                            <div className={styles.name}>Madeup White Cotton Blend Checkered Slim Fit Shirt</div>
-                            <div className={styles.deliveryDate}>Express Delivery by Sat, Aug 30</div>
-                        </div>
-                        <div className={styles.largeScreensOnly}>
-                            <div className={styles.paymentType}>Credit Card Payment</div>
-                            <div className={styles.amount}>Rs 1,899</div>
-                            <div className={styles.control}></div>
-                        </div>
-                    </li>
-                    <div className={styles.break}>
-                        <hr className={styles.linebreak} />
-                    </div>
-                    <li className={styles.listItem}>
-                        <div className={styles.image}>
-                            <img className={styles.image} src={denimModal} alt="denim-shirt-model"></img>
-                        </div>
-                        <div className={styles.details}>
-                            <div className={`${styles.orderId} + " " + ${styles.largeScreensOnly}`}>Order #12345678</div>
-                            <span className={styles.smallScreensOnly}>
-                                <div className={styles.listItem}>
-                                    <div className={styles.orderId}>Order #12345678</div>
-                                    <div className={styles.paymentType}>Cahs on delivery Rs.1899</div>
-                                </div>
-                            </span>
-                            <div className={styles.name}>Madeup White Cotton Blend Checkered Slim Fit Shirt</div>
-                            <div className={styles.deliveryDate}>Express Delivery by Sat, Aug 30</div>
-                        </div>
-                        <div className={styles.largeScreensOnly}>
-                            <div className={styles.paymentType}>Credit Card Payment</div>
-                            <div className={styles.amount}>Rs 1,899</div>
-                            <div className={styles.control}></div>
-                        </div>
-                    </li>
-                    <div className={styles.break}>
-                        <hr className={styles.linebreak} />
-                    </div>
-                    <li className={styles.listItem}>
-                        <div className={styles.image}>
-                            <img className={styles.image} src={denimModal} alt="denim-shirt-model"></img>
-                        </div>
-                        <div className={styles.details}>
-                            <div className={styles.orderId}>Order #12345678</div>
-                            <div className={styles.name}>Madeup White Cotton Blend Checkered Slim Fit Shirt</div>
-                            <div className={styles.deliveryDate}>Express Delivery by Sat, Aug 30</div>
-                        </div>
-                        <div className={styles.largeScreensOnly}>
-                            <div className={styles.paymentType}>Credit Card Payment</div>
-                            <div className={styles.amount}>Rs 1,899</div>
-                            <div className={styles.control}></div>
-                        </div>
-                    </li>
-                    <div className={styles.break}>
-                        <hr className={styles.linebreak} />
-                    </div>
+                    <OrderItem />
                 </ul>
             </div>
         </div>
